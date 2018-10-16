37387c4f5b7472ce336197388163e23abb61959b
Luciffer007 <zerro0770@gmail.com>
2018-10-16 22:43:43 +0300

    MyWordPad version 1.0.0.0

---
+++ App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Globalization;
+
+namespace MyWordPad
+{
+    public partial class App : Application
+    {
+        // List of supported languages
+        private static List<CultureInfo> languagesList = new List<CultureInfo>();
+
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return languagesList;
+            }
+        }
+
+        // Current language
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                // Check the selection of the current application language
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                // Change the current application language
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                // Creating a ResourceDictionary of the new language and connecting the corresponding language resources
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    case "ru-RU":
+                        dict.Source = new Uri("Resources/lang.ru-RU.xaml", UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+
+                // Deleting a ResourceDictionary of a old language and adding ResourceDictionary of a new language
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+            }
+        }
+
+        public App()
+        {
+            // Filling the list of supported languages
+            languagesList.Clear();
+            languagesList.Add(new CultureInfo("en-US"));
+            languagesList.Add(new CultureInfo("ru-RU"));
+
+            Startup += AppStart;
+            Exit += AppExit;
+        }
+
+        // Loading saved language option
+        static void AppStart(object sender, StartupEventArgs e)
+        {
+            Language = MyWordPad.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        // Setting and saving user selected language option
+        static void AppExit(object sender, ExitEventArgs e)
+        {
+            MyWordPad.Properties.Settings.Default.DefaultLanguage = Language;
+            MyWordPad.Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.IO;
+using Microsoft.Win32;
+
+namespace MyWordPad
+{
+    public partial class MainWindow : Window
+    {
+        bool TextChangedFlag;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextChangedFlag = false;
+
+            this.Closing += MainWindowClosing;
+
+            // Setting the default font (12)
+            fontSizeComboBox.SelectedIndex = 4;
+
+            // Setting user selected language option
+            englishCheck.Opacity = MyWordPad.Properties.Settings.Default.DefaultOpacityEng;
+            russianCheck.Opacity = MyWordPad.Properties.Settings.Default.DefaultOpacityRus;
+            textBox.Language = System.Windows.Markup.XmlLanguage.GetLanguage(MyWordPad.Properties.Settings.Default.DefaultLanguage.IetfLanguageTag);
+        }
+
+        // Display information in StatusBar
+        private void DefaultStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("defaultStatusText").ToString();
+        }
+      
+        private void OpenStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("openStatusText").ToString();
+        }
+
+        private void SaveStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("saveStatusText").ToString();
+        }
+
+        private void ExitStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("exitStatusText").ToString();
+        }
+
+        private void CopyStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("copyStatusText").ToString();
+        }
+
+        private void PasteStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("pasteStatusText").ToString();
+        }
+
+        private void CutStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("cutStatusText").ToString();
+        }
+
+        private void LanguageStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("languageStatusText").ToString();
+        }
+
+        private void AboutStatusText(object sender, MouseEventArgs e)
+        {
+            statusText.Text = this.FindResource("aboutStatusText").ToString();
+        }
+
+        // Loading text from a file
+        private void CanOpenExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = this.FindResource("typeDocument").ToString() + " |*.txt";
+            if (true == openFileDialog.ShowDialog())
+            {
+                string dataFromFile = File.ReadAllText(openFileDialog.FileName);
+                textBox.Text = dataFromFile;
+            }
+
+            // Deactivating the text change flag when loading text from a file
+            TextChangedFlag = false;
+        }
+
+        // Saving text to a file
+        private void CanSaveExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = this.FindResource("typeDocument").ToString() + " |*.txt";
+            if (true == saveFileDialog.ShowDialog())
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+
+            // Deactivating the text change flag when saving text to a file
+            TextChangedFlag = false;
+        }
+
+        private void AppExit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Spell Check Assistant
+        private void CheckSpellingHints(object sender, RoutedEventArgs e)
+        {
+            string spellingHints = string.Empty;
+
+            SpellingError error = textBox.GetSpellingError(textBox.CaretIndex);
+            if (error != null)
+                foreach (string s in error.Suggestions)
+                {
+                    spellingHints += string.Format("{0}\n", s);
+                }
+            variantSpellingHints.Content = spellingHints;
+            expandr.IsExpanded = true;
+        }
+
+        // Opening information window about programm
+        private void DisplayInformationWindow(object sender, RoutedEventArgs e)
+        {
+            InfoWindow infoWindow = new InfoWindow();
+            infoWindow.Owner = this;
+            infoWindow.ShowDialog();
+        }
+
+        // Handling event of a font selection for user text
+        private void SelectFontSize(object sender, SelectionChangedEventArgs e)
+        {
+            textBox.FontSize = int.Parse((fontSizeComboBox.SelectedItem as ComboBoxItem).Content.ToString());
+        }
+
+        // Handling event of a selection English localization
+        private void EnglishLanguage(object sender, RoutedEventArgs e)
+        {
+            // Visual demonstration of the selected application language
+            englishCheck.Opacity = 1;
+            russianCheck.Opacity = 0;
+            MyWordPad.Properties.Settings.Default.DefaultOpacityEng = 1;
+            MyWordPad.Properties.Settings.Default.DefaultOpacityRus = 0;
+
+            // Change the current application language to English
+            App.Language = App.Languages[0];
+            textBox.Language = System.Windows.Markup.XmlLanguage.GetLanguage("en-US");
+
+            // Changing localization of a default text of a status bar
+            statusText.Text = "Ready";
+        }
+
+        // Handling event of a selection Russian localization
+        private void RussianLanguage(object sender, RoutedEventArgs e)
+        {
+            englishCheck.Opacity = 0;
+            russianCheck.Opacity = 1;
+            MyWordPad.Properties.Settings.Default.DefaultOpacityEng = 0;
+            MyWordPad.Properties.Settings.Default.DefaultOpacityRus = 1;
+
+            // Change the current application language to Russian
+            App.Language = App.Languages[1];
+            textBox.Language = System.Windows.Markup.XmlLanguage.GetLanguage("ru-RU");
+
+            statusText.Text = "Готово";
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextChangedFlag = true;
+        }
+
+        // Warning message about unsaved data at the time of closing the application
+        private void MainWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (TextChangedFlag)
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to close without saving?", "MyWordPad", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.No)
+                    e.Cancel = true;
+            }
+        }
+    }
+}
